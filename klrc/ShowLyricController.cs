48f88ccdfdcbacc5ae4d7cf60ade15d35b03b5f4
DiodeZ <minhhoang1987@gmail.com>
2015-12-11 00:39:39 +0700

    cap nhat cac thanh chay

--- klrc/ShowLyricController.cs
+++ klrc/ShowLyricController.cs
@@ -18,19 +18,38 @@ namespace klrc
         private List<LineKaraoke> allLyricByLine;
         private karalabel lineOne;
         private karalabel lineTwo;
+        int currentLine = -1;
+        public ShowLyricController()
+        {
+            allLyricByLine = new List<LineKaraoke>();
+        }
         public void showAtTime(double timeInSecond)
         {
+            if (currentLine < allLyricByLine.Count - 2)
+            {
+                if (timeInSecond > allLyricByLine[currentLine + 1].BeginTime - 2)
+                {
+                    currentLine += 1;
+                    Debug.WriteLine(string.Format("Current line playing is {0}/{1}", currentLine+1, allLyricByLine.Count));
+                    if (currentLine % 2 == 0)
+                    {
+                        lineOne.setTextAndTimes(allLyricByLine[currentLine].LyricLRC);
+                    }
+                    else
+                    {
+                        lineTwo.setTextAndTimes(allLyricByLine[currentLine].LyricLRC);
+                    }
+                }
+            }
             try
             {
-                if (lineOne.BeginTime.TotalSeconds <= timeInSecond && lineOne.EndTime.TotalSeconds >= timeInSecond)
+                if (lineOne.BeginTime.TotalSeconds - 1 <= timeInSecond && lineOne.EndTime.TotalSeconds + 1 >= timeInSecond)
                 {
                     lineOne.setCurrentTime(timeInSecond);
-                    return;
                 }
-                if (lineTwo.BeginTime.TotalSeconds <= timeInSecond && lineTwo.EndTime.TotalSeconds >= timeInSecond)
+                if (lineTwo.BeginTime.TotalSeconds - 1 <= timeInSecond && lineTwo.EndTime.TotalSeconds + 1 >= timeInSecond)
                 {
                     lineTwo.setCurrentTime(timeInSecond);
-                    return;
                 }
             }
             catch (Exception ex)
@@ -51,6 +70,106 @@ namespace klrc
         {
             lineOne = line1;
             lineTwo = line2;
+        }
+        public bool readKaraokeLyric(string input)
+        {
+            string realString = "";
+            string timestr = "";
+            TimeSpan beginTime = TimeSpan.FromSeconds(0);
+            TimeSpan tmptime = TimeSpan.FromSeconds(0);
+            int state = 0; //0:read string, 1: read time
+            int lastLinePos = 0;
+            if (input[input.Length - 1] != '\n')
+            {
+                input += "\n";
+            }
+            for (int i = 0; i < input.Length; i++)
+            {
+                if (input[i] == '\n')
+                {
+                    //split line
+                    realString = input.Substring(lastLinePos, i - lastLinePos).Trim();
+                    lastLinePos = i + 1;
+                    if (realString != "")
+                    {
+                        if (i > 0 && input[i - 1] != ']')
+                        {
+                            tmptime += TimeSpan.FromSeconds(1);
+                            realString += string.Format("[{0}]", tmptime.ToString(@"hh\:mm\:ss\.fff"));
+                        }
+                        LineKaraoke tmp = new LineKaraoke();
+                        tmp.BeginTime = beginTime.TotalSeconds;
+                        tmp.EndTime = tmptime.TotalSeconds;
+                        tmp.LyricLRC = realString;
+                        allLyricByLine.Add(tmp);
+                        Console.WriteLine(realString);
+                        state = 0;
+                        beginTime = TimeSpan.FromSeconds(0);
+                        tmptime = TimeSpan.FromSeconds(0);
+                        realString = "";
+                        timestr = "";
+                    }
+                }
+                if (state == 0)
+                {
+                    if (input[i] == '[')
+                    {
+                        state = 1;
+                    }
+                    else
+                    {
+                        realString += input[i];
+                    }
+                }
+                else
+                {
+                    if (input[i] == ']')
+                    {
+                        state = 0;
+                        if (tmptime != TimeSpan.FromSeconds(0))
+                        {
+                            try
+                            {
+                                tmptime = TimeSpan.ParseExact(timestr, "c", null); ;
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                                return false;
+                            }
+                        }
+                        else
+                        {
+                            try
+                            {
+                                tmptime = TimeSpan.ParseExact(timestr, "c", null);
+                                beginTime = tmptime;
+                            }
+                            catch (Exception ex)
+                            {
+                                Debug.WriteLine(ex.Message);
+                                return false;
+                            }
+                        }
+                        timestr = "";
+                    }
+                    else
+                    {
+                        timestr += input[i];
+                    }
+                }
+            }
+            return true;
+        }
+        public override string ToString()
+        {
+            string tmp = "";
+            for (int i = 0; i < allLyricByLine.Count; i++)
+            {
+                tmp += allLyricByLine[i].LyricLRC + "\n";
+
+            }
+            return tmp;
         }
     }
 }
